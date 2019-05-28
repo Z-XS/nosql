46341d4861bbe1d9360a85209fe22daa9fb0ba01
13587503023 <2944235559@qq.com>
2019-05-28 18:13:36 +0800

    5/28

--- admin.js
+++ admin.js
@@ -1,21 +1,113 @@
 var express = require('express')
+var mongoose = require('mongoose')
 var app = express()
 var MongoClient = require('mongodb').MongoClient
 var url = "mongodb://localhost:27017/"
 
-// var connection = MongoClient.connect(url,{useNewUrlParser:true})
+var connection = MongoClient.connect(url,{useNewUrlParser:true})
 
-app.post('/addfoods',function(req,res) {
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,XFILENAME,XFILECATEGORY,XFILESIZE");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+function findcount() {
+    return new Promise((resolve,reject) => {
+        connection.then(db => {
+            var dbo = db.db('manage')
+            dbo.collection('foods').countDocuments().then(count => resolve(count))
+        })
+    })
+}
+var result
+async function Count() {
+    result = await findcount()
+}
+app.get('/getcount',function(req,res) {
+    Count()
+    res.json(result)
+})
+
+app.get('/addfoods',function(req,res) {
+    console.log(req)
+    var arr = [req.query]
+    res.status(200)
+    connection
+    .then(db => {
+        var dbo = db.db('manage')
+        dbo.collection('foods').insertMany(arr,function(err,result) {
+            if(err) throw err
+            res.json('good')
+        })
+    })
+    .catch(err => console.log(err))
+})
+
+app.get('/foodlist',function(req,res) {
     console.log(req.query)
-    // connection
-    // .then(db => {
-    //     var dbo = db.db('manage')
-    //     dbo.connection('foods').insertMany(arr,function(err,result) {
-    //         if(err) throw err
-    //     })
-    // })
-    // .catch(err => console.log(err))
+    if(req.query.search != 'foodlist07') return
+    connection
+    .then(db => {
+        var dbo = db.db('manage')
+        dbo.collection('foods').find({}).toArray(function(err,result) {
+            if(err) throw err
+            res.json(result)
+        })
+    })
+    .catch(err => console.log(err))
+})
 
+app.get('/foodpage',function(req,res) {
+    var pagesize = Number(req.query.pagesize)
+    var page = Number(req.query.page)
+    var skips = Number((page - 1)*pagesize) 
+    connection
+    .then(db => {
+        var dbo = db.db('manage')
+        dbo.collection('foods').find().skip(skips).limit(pagesize).toArray(function(err,result) {
+            if(err) throw err
+            res.json(result)
+        })
+    })
+    .catch(err => console.log(err))  
+})
+
+app.get('/delete',function(req,res) {
+    console.log(req.query)
+    var str = mongoose.Types.ObjectId(req.query.deleteId)
+    console.log(str)
+    const deletestr = {_id : str}
+    connection
+    .then(db => {
+        var dbo = db.db('manage')
+        dbo.collection('foods').deleteOne(deletestr,function(err,result) {
+            if (err) throw err
+            res.json('success')
+        })
+    })
+})
+
+app.get('/update',function(req,res) {
+    var obj = JSON.parse(req.query.form)
+    var str = mongoose.Types.ObjectId(obj._id)
+    const wherestr = {_id : str}
+    console.log(str)
+    delete obj._id
+    console.log(obj)
+    const updatestr = {$set: obj}
+    // const updatestr = {$set: {name: 'skr'}}
+    connection
+    .then(db => {
+        var dbo = db.db('manage')
+        dbo.collection('foods').updateOne(wherestr,updatestr,function(err,result) {
+            if (err) throw err
+            res.json('success')
+        })
+    })
 })
 
 
